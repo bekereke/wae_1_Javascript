8d8b399c327bcda233fa99476fe7004a2fe1832a
Aitor Aretxaga <aitoraretxaga@protonmail.com>
2024-09-24 10:47:52 +0200

    parte2 gehituta '6 ariketari

--- 1-06-array-01/js/jsfunctions.js
+++ 1-06-array-01/js/jsfunctions.js
@@ -6,12 +6,19 @@ document.addEventListener('DOMContentLoaded', function(event) {
 //-------------------------------------------------------------------------------------
 function myFirstArray() {
     //02
-    let myArray=new Array("Lekeitio","Ondarroa","Markina");
+    //let myArray=new Array("Lekeitio","Ondarroa","Markina");
     //03
-    let herriak = parseInt(prompt("Zenbat herri ageri?"));
-    //for(let i=0;i<myArray.length;i++){ //02
-    for(let i=0;i<herriak;i++){ //03
-        console.log(myArray[i]);
+    let myArray=new Array();
+    let herriak;
+    herriak = prompt("Zenbat herri ageri?");
+    herriak = parseInt(herriak);
+
+    for(let i=0;i<herriak;i=i+1){ //02
+        myArray.push(prompt("Sartu "+(i+1)+". herriaren izena"));
     }
 
+    for(let i=0;i<herriak;i=i+1){ //02
+        console.log(myArray[i]);
 }
+}
+
